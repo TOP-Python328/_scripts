ae08286a2701826e401b88d1d4fea8b268db4cf1
Gennadiy <renardes@gmail.com>
2023-07-08 23:31:53 +0500

    виды строковых литералов

---
+++ base/string_literals.py
@@ -0,0 +1,22 @@
+text1 = 'Он сказал: "Сегодня холодно".'
+text2 = "can't do it"
+
+text3 = 'He said: \"it\'s cold today\"'
+
+text4 = 'line 1\nline 2'
+
+text5 = ' \u2192 пункт 1'
+
+text6 = '''Текст на
+нескольких
+строчках'''
+
+text7 = """Текст на
+нескольких
+строчках"""
+
+x, y = 1, 2
+
+equation1 = f'y({x}) = {y}'
+equation2 = f'y({x*2}) = {y**0.5}'
+equation3 = f'y({x*2}) = {y**0.5:.2f}'

d917ac9ec9ce840c67a791c25d271cb9a92c4f3c
Gennadiy <renardes@gmail.com>
2023-07-26 23:19:17 +0500

    примеры создания и использования словарей

---
+++ base/dicts1.py
@@ -0,0 +1,17 @@
+alphabet = {'a': 1, 'b': 2, 'c': 3}
+
+print(type(alphabet))
+print(alphabet, end='\n\n')
+
+print(f'{len(alphabet) = }\n')
+
+
+# >>> list(alphabet.keys())
+# ['a', 'b', 'c']
+
+# >>> list(alphabet.values())
+# [1, 2, 3]
+
+# >>> list(alphabet.items())
+# [('a', 1), ('b', 2), ('c', 3)]
+

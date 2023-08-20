1e8ca3a517a34f5f091588f5757f8bf383a7b1aa
Gennadiy <renardes@gmail.com>
2023-08-21 00:32:47 +0500

    примеры шаблонов сигнатур функций

---
+++ base/functions_templates.py
@@ -0,0 +1,13 @@
+def func_name(pos1, pos2, /, pos_key='default', *, key1, key2):
+    """docstring"""
+
+
+# >>> func_name(1, 2, key1=4, key2=5)
+# >>> 
+
+
+
+def func_name(pos1, /, pos_key, *pos_args, key1='default', **kwargs):
+    """docstring"""
+
+

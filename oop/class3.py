581d0f69c42c8ead5a70477acad671f2af463735
Gennadiy <renardes@gmail.com>
2023-10-09 08:15:55 +0500

    последовательность создания экземпляра

--- oop/class3.py
+++ oop/class3.py
@@ -1,6 +1,6 @@
 class Person:
     # специальный метод конструктор — вызывается во время создания экземпляра
-    def __init__(self, family: str, name: str):
+    def __init__(self, family: str, name: str) -> None:
         # добавление атрибутов экземпляра
         self.last_name = family
         self.first_name = name
@@ -8,6 +8,16 @@ class Person:
 
 ivan = Person('Фекистов', 'Иван')
 anna = Person('Дунаевская', 'Анна')
+
+
+# при вызове объекта класса class_ выполняется метод __call__(), определённый в метаклассе type:
+# def __call__(class_, *args, **kwargs) -> 'instance':
+#     ...
+#     instance = class_.__new__(class_, *args, **kwargs)
+#     ...
+#     instance.__init__(*args, **kwargs)
+#     ...
+#     return instance
 
 # >>> ivan.__dict__
 # {'last_name': 'Фекистов', 'first_name': 'Иван'}

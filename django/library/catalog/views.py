be40e4055ad224dedd09531694082756f0de30b1
Gennadiy <renardes@gmail.com>
2024-02-23 21:13:01 +0500

    представления главной, каталога и страницы автора

--- django/library/catalog/views.py
+++ django/library/catalog/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from transliterate import translit
 
-# Create your views here.
+from catalog.models import Author
+
+
+authors = {
+    translit(author.last_name, 'ru', reversed=True).lower(): author
+    for author in Author.objects.order_by('last_name', 'first_name')
+}
+
+
+def main(request):
+    return render(
+        request,
+        'main.html',
+    )
+
+
+def author_catalog(request):
+    return render(
+        request,
+        'authors.html',
+        {
+            'authors': Author.objects.order_by('last_name', 'first_name'),
+        }
+    )
+
+
+def author(request, name: str):
+    author = authors[name]
+    return render(
+        request, 
+        'author.html',
+        {
+            'author': author,
+            'books': author.book_set.all(),
+        }
+    )
+
